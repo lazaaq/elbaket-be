75fc7b01b710df1e3f7b3effdef06c9ab65fd122
lazaaq <lana.saiful.aqil@mail.ugm.ac.id>
2022-11-18 18:08:28 +0700

    show histories and leaderboard data to material endpoint api

--- app/Http/Controllers/MaterialController.php
+++ app/Http/Controllers/MaterialController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Material;
+use App\QuizCollager;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class MaterialController extends Controller
 {
@@ -13,9 +15,23 @@ class MaterialController extends Controller
     }
 
     public function api_show($quiz_type_id) {
+        $user = Auth::user();
+
         $material = Material::with('media', 'module', 'quiz', 'quiztype.quizCategory')->find($quiz_type_id);
         $materials = Material::where('quiz_type_id', $material['quiz_type_id'])->get()->sortBy('id');
+
+        $histories = QuizCollager::with('answerSave')
+                                ->where('quiz_id', $material->quiz->id)
+                                ->where('collager_id', $user->collager->id)
+                                ->get();
+
+        $leaderboards = QuizCollager::with('collager.user')
+                                    ->get()
+                                    ->sortByDesc('score');
+
         $material['materials'] = $materials;
+        $material['quiz']['histories'] = $histories;
+        $material['quiz']['leaderboards'] = $leaderboards;
         return responseAPI(200, true, $material);
     }
     
